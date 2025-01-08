d67765abe1606b13d629a3ab1eba723a730c0060
Dexter <dexterprimeeight@gmail.com>
2025-01-08 21:05:05 +0300

    добавление свойств в конфиг

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ export default defineNuxtConfig({
     $development: undefined, $env: undefined, $meta: undefined, $production: undefined, $test: undefined,
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
+    nitro: {
+        preset: 'static',
+    },
+    ssr: false,
     modules: [
         '@nuxt/image',
         '@nuxt/eslint',
